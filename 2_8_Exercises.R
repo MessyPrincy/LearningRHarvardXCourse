7fc183afb4eb152b5a1b5fcc5159f1e65f61c74e
MessyPrincy <162635046+MessyPrincy@users.noreply.github.com>
2025-01-25 17:31:06 -0500

    2.8 Exercises

--- 2_8_Exercises.R
+++ 2_8_Exercises.R
@@ -8,3 +8,23 @@ names(temp) <- city
 temp[1:3]
 #5
 temp[c("Paris", "San Juan")]
+#6
+12:72
+#7
+vector <- c(seq(0, 100))
+vector
+#8
+vector <- c(seq(6, 50, 4/7))
+length(vector)
+#9
+a <- seq(1, 10, 0.5)
+class(a)
+#10
+a <- seq(1, 10)
+class(a)
+#11
+class(1L)
+#12
+x <- c("1", "3", "5")
+x <- as.integer(x)
+class(x)
